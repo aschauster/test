f9ec339aef903b73d58ef6f10ce3af1a32a98ea6
aschauster <78444623+aschauster@users.noreply.github.com>
2021-02-16 18:33:35 -0700

    Update submit_sleep
    Needed # before SBATCH commands

--- submit_sleep.sh
+++ submit_sleep.sh
@@ -1,13 +1,13 @@
-!/bin/bash
-SBATCH --account=aschauster
-SBATCH --job-name=sleep
-SBATCH --mail-type=ALL
-SBATCH --mail-user=annie.schauster@ucdenver.edu
-SBATCH --ntasks=1
-SBATCH --nodes=1
-SBATCH --time=0:01:00
-SBATCH --partition=shas-testing
-SBATCH --output=sleep_ID.out
+#!/bin/bash
+#SBATCH --account=aschauster
+#SBATCH --job-name=sleep
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=annie.schauster@ucdenver.edu
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=0:01:00
+#SBATCH --partition=shas-testing
+#SBATCH --output=sleep_ID.out
 
 echo "Running on host" 'aschauster'
 echo "Starting Sleep"
